40716649b77f4940764cc7925daa945cdee091f5
fined <443187@gmail.com>
2018-08-28 04:22:49 +0900

    reindented

--- AviUtlTool/src/main/java/org/oiran/jar/AviUtlTool/App.java
+++ AviUtlTool/src/main/java/org/oiran/jar/AviUtlTool/App.java
@@ -21,105 +21,105 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class App extends JFrame {
-	static App instance;
-	static final int nb_thumbnail = 10;
-	private StatusBar statusBar;
+    static App instance;
+    static final int nb_thumbnail = 10;
+    private StatusBar statusBar;
 
-	public App() {
-		setTitle("kiriharikun");
-		setSize(320, 180);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public App() {
+        setTitle("kiriharikun");
+        setSize(320, 180);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		statusBar = new StatusBar();
-		getContentPane().add(statusBar, BorderLayout.SOUTH);
-	}
+        statusBar = new StatusBar();
+        getContentPane().add(statusBar, BorderLayout.SOUTH);
+    }
 
-	public static void main(String[] args){
-		instance = new App();
-		DropTarget target = new DropTarget(instance, new VideoDropTargetAdapter());
-		instance.setVisible(true);
-	}
+    public static void main(String[] args){
+        instance = new App();
+        DropTarget target = new DropTarget(instance, new VideoDropTargetAdapter());
+        instance.setVisible(true);
+    }
 
-	public void exit() {
-		instance.dispatchEvent(new WindowEvent(instance, WindowEvent.WINDOW_CLOSING));
-	}
+    public void exit() {
+        instance.dispatchEvent(new WindowEvent(instance, WindowEvent.WINDOW_CLOSING));
+    }
 
-	public void setStatusMessage(String message) {
-		statusBar.setMessage(message);
-	}
+    public void setStatusMessage(String message) {
+        statusBar.setMessage(message);
+    }
 }
 class StatusBar extends JLabel {
-	public StatusBar() {
-		super();
-		super.setPreferredSize(new Dimension(100, 16));
-		setMessage("");
-	}
+    public StatusBar() {
+        super();
+        super.setPreferredSize(new Dimension(100, 16));
+        setMessage("");
+    }
 
-	public void setMessage(String message) {
-		setText(message);
-	}
+    public void setMessage(String message) {
+        setText(message);
+    }
 }
 class VideoDropTargetAdapter extends DropTargetAdapter{
-	static List<String> paramToSendList;
-	static {
-		String[] paramToSend = {
-				"width",
-				"height",
-				"r_frame_rate",
-				"duration",
-				"nb_frames",
-				"filename",
-				"size"
-		};
-		paramToSendList = Arrays.asList(paramToSend);
-	}
-	public void drop(DropTargetDropEvent dtde) {
-		try {
-			Transferable transfer = dtde.getTransferable();
-			if (transfer.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
-				dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
-				List<File> fileList = (List<File>) transfer.getTransferData(DataFlavor.javaFileListFlavor);
-				if (fileList.size() == 1) {
-					File f = fileList.get(0);
-					if (f.getName().endsWith(".mp4")) {
-						Mp4 mp4 = new Mp4(f);
-						if (mp4.loadMetadata()) {
-							File ft = new File(getClass().getResource("/html/tn").getPath());
-							//if (mp4.createThumbnails(ft)) {
-							Desktop desktop = Desktop.getDesktop();
-							File fp = new File(getClass().getResource("/html/index.html").getPath());
-							File fj = new File(getClass().getResource("/html/data.js").getPath());
-							String q = "";
-							for (Entry<String, String> entry : mp4.metadata.entrySet()) {
-								if (paramToSendList.contains(entry.getKey())) {
-									q += "&" + entry.getKey() + "=" + entry.getValue();
-								}
-							}
-							if (q.length() < 1) {
-								System.err.println("Not enough metadatas");
-								return;
-							}
-							FileWriter fw = new FileWriter(fj);
-							BufferedWriter bw = new BufferedWriter(fw);
-							bw.write("const JAVA_QUERY = '"+escape(q.substring(1))+"'");
-							bw.close();
-							desktop.browse(fp.toURI());
-							App.instance.exit();
-							//} else {
-							//	System.err.println("failed to create thumbnails");
-							//}
-						} else {
-							System.err.println("failed to load video metadatas");
-						}
-					}
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	public String escape(String s){
-		return s.replaceAll("'", "&apos;").replaceAll("\\\\", "\\\\\\\\");
-	}
+    static List<String> paramToSendList;
+    static {
+        String[] paramToSend = {
+                "width",
+                "height",
+                "r_frame_rate",
+                "duration",
+                "nb_frames",
+                "filename",
+                "size"
+        };
+        paramToSendList = Arrays.asList(paramToSend);
+    }
+    public void drop(DropTargetDropEvent dtde) {
+        try {
+            Transferable transfer = dtde.getTransferable();
+            if (transfer.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                dtde.acceptDrop(DnDConstants.ACTION_COPY_OR_MOVE);
+                List<File> fileList = (List<File>) transfer.getTransferData(DataFlavor.javaFileListFlavor);
+                if (fileList.size() == 1) {
+                    File f = fileList.get(0);
+                    if (f.getName().endsWith(".mp4")) {
+                        Mp4 mp4 = new Mp4(f);
+                        if (mp4.loadMetadata()) {
+                            File ft = new File(getClass().getResource("/html/tn").getPath());
+                            //if (mp4.createThumbnails(ft)) {
+                            Desktop desktop = Desktop.getDesktop();
+                            File fp = new File(getClass().getResource("/html/index.html").getPath());
+                            File fj = new File(getClass().getResource("/html/data.js").getPath());
+                            String q = "";
+                            for (Entry<String, String> entry : mp4.metadata.entrySet()) {
+                                if (paramToSendList.contains(entry.getKey())) {
+                                    q += "&" + entry.getKey() + "=" + entry.getValue();
+                                }
+                            }
+                            if (q.length() < 1) {
+                                System.err.println("Not enough metadatas");
+                                return;
+                            }
+                            FileWriter fw = new FileWriter(fj);
+                            BufferedWriter bw = new BufferedWriter(fw);
+                            bw.write("const JAVA_QUERY = '"+escape(q.substring(1))+"'");
+                            bw.close();
+                            desktop.browse(fp.toURI());
+                            App.instance.exit();
+                            //} else {
+                            //    System.err.println("failed to create thumbnails");
+                            //}
+                        } else {
+                            System.err.println("failed to load video metadatas");
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public String escape(String s){
+        return s.replaceAll("'", "&apos;").replaceAll("\\\\", "\\\\\\\\");
+    }
 }
--- AviUtlTool/src/main/java/org/oiran/jar/AviUtlTool/Mp4.java
+++ AviUtlTool/src/main/java/org/oiran/jar/AviUtlTool/Mp4.java
@@ -9,14 +9,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Mp4 {
-	File file;
-	Map<String, String> metadata;
+    File file;
+    Map<String, String> metadata;
 
-	public Mp4(File file) {
-		this.file = file;
-	}
+    public Mp4(File file) {
+        this.file = file;
+    }
 
-	boolean loadMetadata() {
+    boolean loadMetadata() {
         Runtime runtime = Runtime.getRuntime();
 
         String[] command = {"cmd", "/c", "ffprobe -loglevel quiet -show_format -show_streams \"" + file.getAbsolutePath() + "\""};
@@ -24,81 +24,81 @@ public class Mp4 {
         File cwd = new File(getClass().getResource("/ffmpeg/bin/").getPath());
 
         Process p;
-		try {
-			p = runtime.exec(command, null, cwd);
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
+        try {
+            p = runtime.exec(command, null, cwd);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
 
         InputStream is = p.getInputStream();
         BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
         metadata = new HashMap<String, String>();
 
-		try {
-	        while (true) {
-	            String line = br.readLine();
-	            if (line == null) {
-	                break;
-	            } else {
-	            	if (line.matches("^[^=]+=[^=]+$")) {
-	            		String[] s = line.split("=");
-	            		metadata.put(s[0], s[1]);
-	            	}
-	            }
-	        }
-		} catch (IOException e) {
-			e.printStackTrace();
-			return false;
-		}
+        try {
+            while (true) {
+                String line = br.readLine();
+                if (line == null) {
+                    break;
+                } else {
+                    if (line.matches("^[^=]+=[^=]+$")) {
+                        String[] s = line.split("=");
+                        metadata.put(s[0], s[1]);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
 
         if (metadata.isEmpty()) {
-        	return false;
+            return false;
         }
 
         return true;
-	}
+    }
 
-	boolean createThumbnails(File tnDir) {
-		int tn_interval = 0;
-		if (this.metadata != null && this.metadata.containsKey("duration")) {
-			String durString = this.metadata.get("duration");
-			if (durString.matches("^[0-9.]+$")) {
-				double sec = Double.parseDouble(durString);
-				tn_interval = (int) Math.floor(sec / App.nb_thumbnail);
-			}
-		}
+    boolean createThumbnails(File tnDir) {
+        int tn_interval = 0;
+        if (this.metadata != null && this.metadata.containsKey("duration")) {
+            String durString = this.metadata.get("duration");
+            if (durString.matches("^[0-9.]+$")) {
+                double sec = Double.parseDouble(durString);
+                tn_interval = (int) Math.floor(sec / App.nb_thumbnail);
+            }
+        }
 
-		if (tn_interval == 0) {
-			System.err.println("no duration metadata");
-			return false;
-		}
-		ProcessBuilder pb = new ProcessBuilder(
-				"ffmpeg",
-				"-i", file.getAbsolutePath(),
-				"-vf", "fps=1/" + tn_interval,
-				"\""+tnDir.getAbsolutePath() + File.separator + "thumb%03d.jpg\""
-				);
+        if (tn_interval == 0) {
+            System.err.println("no duration metadata");
+            return false;
+        }
+        ProcessBuilder pb = new ProcessBuilder(
+                "ffmpeg",
+                "-i", file.getAbsolutePath(),
+                "-vf", "fps=1/" + tn_interval,
+                "\""+tnDir.getAbsolutePath() + File.separator + "thumb%03d.jpg\""
+                );
 
-		File cwd = new File(getClass().getResource("/ffmpeg/bin/").getPath());
+        File cwd = new File(getClass().getResource("/ffmpeg/bin/").getPath());
 
-		System.out.println("creating thumnails...");
+        System.out.println("creating thumnails...");
 
         try {
-        	App.instance.setStatusMessage("creating thumnails...");
-        	pb.directory(cwd);
-        	Process p = pb.start();
+            App.instance.setStatusMessage("creating thumnails...");
+            pb.directory(cwd);
+            Process p = pb.start();
             BufferedReader stdOut = new BufferedReader(new InputStreamReader(p.getInputStream()));
             String s;
             while((s = stdOut.readLine()) != null){
-            	System.out.println(s);
+                System.out.println(s);
             }
-        	App.instance.setStatusMessage("");
+            App.instance.setStatusMessage("");
         } catch (Exception e) {
             e.printStackTrace();
             return false;
         }
         return true;
-	}
+    }
 }
